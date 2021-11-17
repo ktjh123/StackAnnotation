5c9eeb8adbffe930d0caa7fae52f52858e253675
ktjh123 <37207972+ktjh123@users.noreply.github.com>
2021-11-17 10:31:54 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -44,6 +44,8 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;
 
 		MyStringStack stack = new MyStringStack();
+		stack.push (s1);
+		assertTrue (stack.isEmpty());
         assertTrue ("Initial Stack state", stack.isEmpty());
         try {
             o1 = stack.pop ();
